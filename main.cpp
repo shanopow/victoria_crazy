0f170158ff3844ef6cf2d12832a89478d27d6806
shanopow <115043900+shanopow@users.noreply.github.com>
2022-10-25 11:50:22 +0100

    Added basic supply and demand
    wiluraghoWIUGNOWIRHUIOtgtil.AK

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include <iterator>
-#include <list>
 using namespace std;
 
-class Province{
+class Market {
     public:
-    float terrain;
-    float output_amnt;
-    float inf_pres;
-    float ar_pres;
-    Province(float terrain1, float output_amnt1, float inf_pres1, float ar_pres1){
+    float p_grain; float p_fish; float p_fruit; float p_cattle; 
+    float p_tea; float p_coffee; float p_clothes; float p_furniture;
+    float p_liquor; float p_paper; float p_car; float p_fuel;
+    Market(){
+    p_grain = 3;
+    p_fish = 4;
+    p_fruit = 4;
+    p_cattle = 4;
+    p_tea = 5;
+    p_coffee = 9;
+    p_clothes = 14;
+    p_furniture = 17;
+    p_liquor = 8;
+    p_paper = 5.6;
+    p_car = 20;
+    p_fuel = 8;
+    }
+};
+
+class Province {
+    public:
+    string rgo_type; float terrain; float output_amnt; float inf_pres; float ar_pres;
+    float production = 0;
+    
+    Province(){}
+    Province(string rgo_type1, float terrain1, float output_amnt1, float inf_pres1, float ar_pres1){
+        rgo_type = rgo_type1;
         terrain = terrain1;
         output_amnt = output_amnt1;
         inf_pres = inf_pres1;
@@ -35,30 +56,51 @@ class Farmer {
 
 };
 
+// rgo
 float production(float a_pres, float inf_pres ,float terrain, float output_amnt);
 float base(float terrain, float output_amnt);
 float output_eff(float terrain, float a_pres, float inf_pres);
 
+//pop demand and supply
+float demander(string demand_type, string demand_amount, Farmer arr[], int sizearr);
+float supplier();
+
 int main(){
+    // market created
+    Market m;
     //what each pop consumes
+    // move to header file
     string farmer_consumes [3][5][2] = { 
     { {"grain","0.75"},{"fish","1"},{"fruit","1"},{"cattle","0.75"},{"null","0"} },
     { {"tea","4"},{"liquor","3"},{"clothes","1.2"},{"furniture","1.1"},{"null","0"} },
     { {"furniture","3"},{"clothes","3"},{"liquor","1.5"},{"paper","1"},{"coffee","0.45"} } 
     };
 
-    // farmer holders
+    // farmers in state
+    // linked list maybe
     Farmer farmers[100];
     for(int i = 0; i < 100; i++){
         farmers[i] = Farmer(i);
-        farmers[i].printer();
     }
-    printf("Welcome to the province of Berlin in Brandenburg\n");
-    printf("This PLAINS area produces GRAIN\n");
+    
     // rgo calc
-    Province berlin(1, 1.8, 0.2, 0.02);    
-    float total = production(berlin.ar_pres, berlin.inf_pres, berlin.terrain, berlin.output_amnt);
-    cout << total;
+    Province berlin("grain", 1, 1.8, 0.2, 0.02);
+    berlin.production = production(berlin.ar_pres, berlin.inf_pres, berlin.terrain, berlin.output_amnt);
+
+    // demands of pops in state
+    int sizer = (sizeof(farmers)/sizeof(*farmers));
+    float demand;
+    for (int i = 0; i < 12; i++){
+        demand = demander(farmer_consumes[0][i][0], farmer_consumes[0][i][1] , farmers, sizer);
+        //more supply than demand
+        if (demand < berlin.production){
+            m.p_grain -= 0.01; 
+        }
+        else{
+            m.p_grain += 0.01;
+        }
+    }
+    cout << m.p_grain;
     return 0;
 }
 
@@ -73,6 +115,17 @@ float base(float terrain, float output_amnt){
 }
 
 // calcs output efficiency (1 + arisocrats% + infrastructure% + terain)
-float output_eff(float terrain, float a_pres, float inf_pres){
+float output_eff(float terrain, float a_pres, float inf_pres){ 
     return 1 + a_pres + terrain + inf_pres;
+}
+
+// calcs the demand in a province for certain good
+float demander(string demand_type, string demand_amount, Farmer arr[], int sizearr){
+    float tot_demand = 0;
+    float rgo_d_amnt = stof(demand_amount);
+    for (int i = 0 ; i < sizearr; i++){
+        tot_demand += rgo_d_amnt;
+    }
+    cout << demand_type << " " << tot_demand << "\n";
+    return tot_demand;
 }
